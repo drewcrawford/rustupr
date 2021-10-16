ca4110ab8220683eeeb2756e15cfddee28fadd1e
Drew Crawford <drew@sealedabstract.com>
2021-10-16 14:42:50 -0500

    Windows install

--- src/install_rustup.rs
+++ src/install_rustup.rs
@@ -1,5 +1,7 @@
 use command_rs::{Command, ExitStatus};
 use crate::Error;
+use pcore::release_pool::ReleasePool;
+use std::path::PathBuf;
 
 async fn update_rustup(priority: kiruna::Priority) -> Result<(),Error> {
     Command::new("rustup")
@@ -9,15 +11,42 @@ async fn update_rustup(priority: kiruna::Priority) -> Result<(),Error> {
 }
 
 async fn is_rustup_installed(priority: kiruna::Priority) -> bool {
-    Command::new("rustup").status(priority).await.map_or_else(|_| false,|_| true)
+    #[cfg(target_os = "macos")] {
+        Command::new("rustup").status(priority).await.map_or_else(|_| false, |_| true)
+    }
+    #[cfg(target_os = "windows")] {
+        false //todo!
+    }
 }
 async fn install_rustup(priority: kiruna::Priority) -> Result<(),Error> {
-    let status = Command::new("/bin/sh")
-        .arg("-c")
-        .arg("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
-        .status(priority).await?;
-    status.check_err().map_err(|e| e.into())
+    #[cfg(target_os = "macos")] {
+        let status = Command::new("/bin/sh")
+            .arg("-c")
+            .arg("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
+            .status(priority).await?;
+        status.check_err().map_err(|e| e.into())
+    }
+   #[cfg(target_os="windows")] {
+       //we're on windows, so eh
+       let pool = unsafe{ReleasePool::assuming_pool()};
+       let download_request = requestr::Request::new(pcore::pstr!("https://win.rustup.rs/x86_64"), pool)?;
+       let download = download_request.download(pool).await?;
+       let path = download.copy_path();
+       /*see https://github.com/rust-lang/rustup/blob/44be718122ecff073bcb2dfd44c6b50ed84c7696/src/bin/rustup-init.rs#L100-L109, but the file
+       actually needs to be named 'rustup.exe' for some reason
+        */
+       let mut new_path = path.clone();
+       new_path.pop();
+       new_path.push("rustup-init.exe");
+       std::fs::rename(path,new_path.clone()).unwrap();
+       std::mem::forget(download); //don't bother cleaning up tempfile; it was moved
+       let status = Command::new(new_path)
+           .arg("-y")
+           .status(priority).await?;
+       status.check_err().map_err(|e| e.into())
+   }
 }
+
 pub async fn ensure_rustup(priority: kiruna::Priority) -> Result<(),Error> {
     if !is_rustup_installed(priority).await {
         install_rustup(priority).await?;
