dfb82117fb4307c13ed779123fd1a4506866604b
Drew Crawford <drew@sealedabstract.com>
2021-10-16 13:49:20 -0500

    Update for dependencies

--- src/install_rustup.rs
+++ src/install_rustup.rs
@@ -1,32 +1,34 @@
-use command_rs::Command;
+use command_rs::{Command, ExitStatus};
 use crate::Error;
 
-async fn update_rustup() -> Result<(),Error> {
+async fn update_rustup(priority: kiruna::Priority) -> Result<(),Error> {
     Command::new("rustup")
         .arg("update")
-        .status().await?;
+        .status(priority).await?;
     Ok(())
 }
 
-async fn is_rustup_installed() -> bool {
-    Command::new("rustup").status().await.map_or_else(|_| false,|_| true)
+async fn is_rustup_installed(priority: kiruna::Priority) -> bool {
+    Command::new("rustup").status(priority).await.map_or_else(|_| false,|_| true)
 }
-
-pub async fn ensure_rustup() -> Result<(),Error> {
-    if !is_rustup_installed().await {
-        let r = Command::new("/bin/sh")
-            .arg("-c")
-            .arg("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
-            .status().await;
-        r?;
+async fn install_rustup(priority: kiruna::Priority) -> Result<(),Error> {
+    let status = Command::new("/bin/sh")
+        .arg("-c")
+        .arg("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
+        .status(priority).await?;
+    status.check_err().map_err(|e| e.into())
+}
+pub async fn ensure_rustup(priority: kiruna::Priority) -> Result<(),Error> {
+    if !is_rustup_installed(priority).await {
+        install_rustup(priority).await?;
     }
     else {
-        update_rustup().await?;
+        update_rustup(priority).await?;
     }
     Ok(())
 }
 #[test] fn test_rustup() {
-    let f = ensure_rustup();
-    let a = kiruna::test::test_await(f, std::time::Duration::from_secs(20));
+    let f = ensure_rustup(kiruna::Priority::Testing);
+    let a = kiruna::test::test_await(f, std::time::Duration::from_secs(180));
     a.unwrap()
 }
